593f1290fe1cd531a11f84a05b6f05e1b0db8f47
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-23 16:00:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
 # your code goes here
+def begins_with_r(array)
+array.each do |element|
+  if element.start_with?("r")
+   return true
+  elsif element.!start_with?("r")
+  return false
+else 
+  false
+  
+  end
+end
+
+end
